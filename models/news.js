4d896d6349e0e4e49b4a51064f27c88cde6e7905
lkyyyy123456 <14228356+lkyyyy123456@user.noreply.gitee.com>
2025-04-13 15:08:10 +0800

    feat:修改表名

--- models/news.js
+++ models/news.js
@@ -1,7 +1,7 @@
 const {DataTypes}=require('sequelize')
 const sequelize = require('../config/db')
 
-const News=sequelize.define('News',{
+const News=sequelize.define('news',{
     sub: DataTypes.STRING,
     title: DataTypes.STRING,
     time: DataTypes.JSON,
